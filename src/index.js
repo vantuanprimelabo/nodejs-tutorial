07397fe1725323e88ea478c1f877aa222465960a
vantuanprimelabo <van.tuan@primelabo.com.vn>
2018-08-01 16:42:37 +0700

    event in Nodejs

--- src/index.js
+++ src/index.js
@@ -6,25 +6,45 @@
 //
 // console.log('Server running at http://127.0.0.1:1337/');
 // console.log('Met qua troi oi');
-import fileManager from './fileManager';
 
-const fileName = __dirname + '/../data/temp.txt';
-fileManager.createNewFile(fileName);
-let jsonObject = {
-    foods: [
-        {
-            "name": "aaa",
-            "title": "b"
-        },
-        {
-            "name": "dd",
-            "title": "ff"
-        },
-    ],
-    statusCode: 200,
-    fullName: "tuandz"
-};
-fileManager.saveJsonToFile(jsonObject, fileName);
-jsonObject = {...jsonObject, address: "aaaaaa"};
-fileManager.saveJsonToFile(jsonObject, fileName);
-fileManager.readFileData(fileName);
+
+// import fileManager from './fileManager';
+//
+// const fileName = __dirname + '/../data/temp.txt';
+// fileManager.createNewFile(fileName);
+// let jsonObject = {
+//     foods: [
+//         {
+//             "name": "aaa",
+//             "title": "b"
+//         },
+//         {
+//             "name": "dd",
+//             "title": "ff"
+//         },
+//     ],
+//     statusCode: 200,
+//     fullName: "tuandz"
+// };
+// fileManager.saveJsonToFile(jsonObject, fileName);
+// jsonObject = {...jsonObject, address: "aaaaaa"};
+// fileManager.saveJsonToFile(jsonObject, fileName);
+// fileManager.readFileData(fileName);
+
+const Event = require('events');
+class Customer extends Event {
+    constructor(name, gender) {
+        super();
+        this.name = name;
+        this.gender = gender;
+    }
+}
+const mrJohn = new Customer('John', 'male');
+const callBackFuntion = (foods, customer) => {
+    for(let index in foods) {
+        console.log(`${customer.name} call ${foods[index]}`);
+    }
+}
+mrJohn.on('eventCallFood', callBackFuntion);
+console.log('watting...........');
+mrJohn.emit('eventCallFood',['mitom','trung ga'], mrJohn);
